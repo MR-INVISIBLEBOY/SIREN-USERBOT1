ceed5c19f66d3dd7da63976184b02d9a9092401e
Mr_Invisible_Boy <101985120+MR-INVISIBLEBOY@users.noreply.github.com>
2022-04-21 09:04:49 -0700

    Create SIREN-ROOTFILE.js

---
+++ SIREN-ROOTFILE.js
@@ -0,0 +1,15 @@
+#Clone_Tha_Repo_Side_Mode_By_Legendhacker_IIN_&_InvisibleBoy
+Run git.MR-INVISIBLEBOY/SIRENUSERBOT/root.directy
+FROM GITHUB.NONE,.TYPES ":OMK:"
+Legend-SirenUserBot = await:Legend_Invisible)(int({str(LegendUserBot/root/directy Fun&Clone>/n"
+         [
+                    os.event.get[
+                                "string_sessoin"
+                     await = sleep_time(^&8^&none,).get
+                     await:OMK:"LegendBot_Owner(@LegendHacker_IIN) 
+                                                   int([OWNER])0.sleeping_Flood wait;
+                                             in(os.(event:)"print(str(velue_input(await:print(os.event)"not/n"
+   
+ 
+  f"      LegendBot_Of Cyber supporting On google /n"
+  f"           Alive_msg = "legendBot_new" /n"
